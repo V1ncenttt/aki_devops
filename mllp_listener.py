acd100a20fd2eef542600198c1e36b9acad53c25
KGB99 <k.birgi@gmail.com>
2025-02-06 20:28:35 +0000

    change behaviour of run loop

--- mllp_listener.py
+++ mllp_listener.py
@@ -23,8 +23,29 @@ class MllpListener:
         self.mllp_port = mllp_port
         self.msg_queue = msg_queue
         self.client_socket = None
+        self.open_connection()
     
-    def close_connection(self):
+    def open_connection(self):
+        while True:
+            try:
+                logging.info(f"[*] Connecting to HL7 Simulator at {SIMULATOR_HOST}:{SIMULATOR_PORT}...")
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket.settimeout(10)
+                client_socket.connect((SIMULATOR_HOST, SIMULATOR_PORT))
+                self.client_socket = client_socket
+                logging.info("[+] Connected to HL7 Simulator!")
+                break
+            except (ConnectionRefusedError, ConnectionResetError):
+                logging.error(f"[-] Could not connect to {SIMULATOR_HOST}:{SIMULATOR_PORT}, retrying in 5s...")
+                time.sleep(5)
+        return
+
+
+
+    def shutdown(self):
+        """
+        for closing the connection
+        """
         self.client_socket.close()
         logging.info("[*] Connection closed. Quitting...")
         # i feel like we should exit the system here at this point, see about it later tbh
@@ -34,22 +55,20 @@ class MllpListener:
 
     def hl7_listen(self):
             """Listen for HL7 messages, process them, and assign workers."""
-            logging.info(f"[*] Connecting to HL7 Simulator at {SIMULATOR_HOST}:{SIMULATOR_PORT}...")
 
-            try:
-                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client_socket.settimeout(10)
-                client_socket.connect((SIMULATOR_HOST, SIMULATOR_PORT))
-                self.client_socket = client_socket
-                logging.info("[+] Connected to HL7 Simulator!")
+                #client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                #client_socket.settimeout(10)
+                #client_socket.connect((SIMULATOR_HOST, SIMULATOR_PORT))
+                #self.client_socket = client_socket
+                #logging.info("[+] Connected to HL7 Simulator!")
 
-                buffer = b""
-
+            buffer = b""
+            while True:
                 try:
-                    data = client_socket.recv(1024)
+                    data = self.client_socket.recv(1024)
                     if not data:
                         logging.info("[-] No more data, closing connection.")
-                        self.close_connection()
+                        self.shutdown()
 
                     buffer += data
 
@@ -59,35 +78,32 @@ class MllpListener:
                         hl7_message = buffer[start_index:end_index].decode("utf-8").strip()
                         buffer = buffer[end_index + len(END_BLOCK) :]
 
+                        #currently everything is being parsed here, we can modularize this if we wish to make it even more modular
                         parsed_message = self.parser.parse(hl7_message)
                         
                         
                         if parsed_message is None or parsed_message[0] is None:
                             logging.error("Received invalid HL7 message or unknown message type.")
                             break  # Prevents further errors
-                        #elif parsed_message[0] == "ORU^R01":
-                        #    self.process_oru_message(parsed_message)
-                        #elif parsed_message[0] == "ADT^A01":
-                        #    self.process_adt_message(parsed_message)
 
                         # TODO: Write to msg_queue
                         self.msg_queue.append(parsed_message)
 
 
                         ack_message = self.parser.generate_hl7_ack(hl7_message)
-                        client_socket.sendall(ack_message)
+                        self.client_socket.sendall(ack_message)
                         logging.info(f"[ACK SENT]")
+                        return
 
                 except socket.timeout:
                     logging.warning("[-] Read timeout. Closing connection.")
 
                 #added this to another function as well, dunno bruh
-                client_socket.close()
-                logging.info("[*] Connection closed. Quitting...")
-                return
-                
-
-            except (ConnectionRefusedError, ConnectionResetError):
-                logging.error(f"[-] Could not connect to {SIMULATOR_HOST}:{SIMULATOR_PORT}, retrying in 5s...")
-                time.sleep(5)
+                #self.client_socket.close()
+                #logging.info("[*] Connection closed. Quitting...")
+                #return
+    
+    def run(self):
+        self.hl7_listen()
+        return
             
