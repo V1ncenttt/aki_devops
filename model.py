9fec96f2c346d1a79ec8818d3a0cd9ee22e5b06d
KGB99 <k.birgi@gmail.com>
2025-02-07 02:03:38 +0000

    update model to be used

--- model.py
+++ model.py
@@ -6,154 +6,100 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from joblib import dump, load
+#from xgboost import XGBClassifier
 import numpy as np
 import json 
 from pandas_database import PandasDatabase
 import logging
 from database import Database
+from sklearn.preprocessing import LabelEncoder
 
 
-######################################
-
-class SimpleNN(nn.Module):
-        def __init__(self, input_size, hidden_size):
-            super(SimpleNN, self).__init__()
-            self.hidden = nn.Linear(input_size, hidden_size)
-            self.activation_hidden = nn.ReLU()  # ReLU for hidden layer
-            self.output = nn.Linear(hidden_size, 1)
-            self.activation_output = nn.Sigmoid() # Sigmoid for binary classification
-
-        def forward(self, x):
-            x = self.activation_hidden(self.hidden(x))
-            x = self.activation_output(self.output(x))
-            return x
 
 class Model:
     """_summary_"""
 
     def __init__(self, predict_queue):
-        """_summary_
+        self.predict_queue = predict_queue
+        self.aki_model = load('aki_detection.joblib')
+        self.le = LabelEncoder()
+
+
+    def add_padding(self, df):
+        """
+        Add padding to dataframe to ensure constant length
+        
+        Args:
+            df (Dataframe): Dataframe to be formatted
+
+        Returns:
+            Dataframe : Dataframe with constant measurement length of 50
+        """
+        for i in range(((len(df.columns)-2)//2), 50):
+            df[f'creatinine_date_{i}'] = 0
+            df[f'creatinine_result_{i}'] = 0
+        return df
+    
+    def process_dates(self, df):
+        """
+        Process dates from the format of 'Year-Month-Day Hour-Minutes-Seconds' into seconds.
+        0 is the time of the first measurement and all following values are in relation to the first measurement.
+        
+        Args:
+            df (Dataframe): Dataframe with dates in 'Year-Month-Day Hour-Minutes-Seconds' format
+
+        Returns:
+            Dataframe : Dataframe with dates in seconds (int)
+        """
+        date_cols = [col for col in df.columns if "creatinine_date" in col]
+        for col in date_cols:
+            df[col] = pd.to_datetime(df[col], errors='coerce')
+        df['ref_date'] = df[date_cols].min(axis=1)
+        for col in date_cols:
+            df[col] = (df[col] - df['ref_date']).dt.total_seconds()
+        df = df.drop(columns=['ref_date'])
+        df = df.fillna(0)
+        return df
+
+    def process_features(self, df):
+        """
+        Process features of input data for detection of aki. First the dates are turned into seconds (int) and then padding is added to ensure constant length
+        of input data. Finally some extra aggregated feature values are computed.
 
         Args:
-            
+            df (Dataframe): Dataframe with input data
+
+        Returns:
+            Dataframe : Dataframe with processed features
         """
-        self.predict_queue = predict_queue
-        with open('expected_columns.json', 'r') as f:
-            self.expected_columns = json.load(f)
-        self.expected_columns_len = len(self.expected_columns)  # Ensure we get the correct number of features
-        self.model = SimpleNN(input_size=self.expected_columns_len, hidden_size=64)  # Match training definition
-        self.model.load_state_dict(torch.load('model.pth', weights_only=True))  # Load trained weights
-        self.model.eval()
+        results_cols = [col for col in df.columns if "creatinine_result" in col]
+
+        #df = self.process_dates(df)
+        #df = self.add_padding(df)
 
 
-        
-    def test_preprocessing(self, measurement_row, expected_columns):
-        """
-            Preprocess input of the network. Runs on training data with AKI column.
+        df['creatinine_mean'] = df[results_cols].mean(axis=1)
+        df['creatinine_median'] = df[results_cols].median(axis=1, skipna=True)
+        df['creatinine_max'] = df[results_cols].max(axis=1)
+        df['creatinine_min'] = df[results_cols].min(axis=1)
+        df["creatinine_max_delta"] = df[results_cols].diff(axis=1).max(axis=1).fillna(0) # note: this is better than df['creatinine_max'] - df['creatinine_min']
+        df['creatinine_std'] = df[results_cols].std(axis=1)
+        df['most_recent'] = df[results_cols].apply(lambda row: row.dropna().iloc[-1] if not row.dropna().empty else None, axis=1)
+        df['rv1_ratio'] = df['most_recent'] / df['creatinine_min']
+        df['rv2_ratio'] = df['most_recent'] / df['creatinine_median']
+        df.drop(columns="")
+        return df
 
-            Arguments:
-                - training_filepath {string} -- csv file path # TODO: CHANGE THIS DESCRIPTION
-                - expected_columns {list} -- Expected columns based on the training data
-                    
-
-            Returns:
-                - {torch.tensor}  -- Preprocessed input array 
-                - {list} -- Expected labels (when using test.csv). 
-                
-            """
-            
-        
-        # Using measurment row
-        df = measurement_row
-        
-        
-        # Make sure that mrn column is either dropped or not inputted! 
-        if 'mrn' in df.columns:
-            df = df.drop(columns=['mrn'])
-        
-        df['sex'] = df['sex'].map({'f': 0, 'm': 1})
-        for col in df.columns:
-            if 'creatinine_date' in col:
-                df[col] = pd.to_datetime(df[col],format= '%m/%d/%y %H:%M:%S', errors='coerce')  # Convert to datetime
-
-        # This is only used when running on personal environment, where test.csv includes ground truth labels.
-        if 'aki' in df.columns:
-            df['aki'] = df['aki'].map({'n': 0, 'y': 1}) # Convert to binary 
-            y = df['aki'].values  # Extract labels
-        else: y = 0
-        
-        # Fill nan values with 0
-        df = df.apply(pd.to_numeric, errors='coerce')  # Convert all columns to numeric, replacing invalids with NaN
-        df.fillna(0, inplace=True)
-        
-        # 🔹 Extract the same creatinine result columns as training
-        creatinine_columns = [col for col in df.columns if 'creatinine_result' in col]
-        
-        # 🔹 Add derived features: median & max creatinine values 
-        df['creatinine_median'] = df[creatinine_columns].median(axis=1)
-        df['creatinine_max'] = df[creatinine_columns].max(axis=1)
-        
-        # Match test data size with training data size if it is larger 
-        expected_columns_len = len(expected_columns)
-
-        df_reversed = df.iloc[:, ::-1]  # Reverse column order so that mose recent test dates 
-        
-        df_reversed = df_reversed.iloc[:, :expected_columns_len]  # Keeps only the first `expected_columns_len` columns
-        
-        
-        # Pad if test is smaller than training
-        for col in expected_columns:
-            if col not in df.columns:
-                df[col] = 0  # pad with 0
-        df = df[expected_columns]
-        
-        
-        # Convert to tensors
-        X = df_reversed
-        X = X.to_numpy()  
-        y = np.array(y) 
-        X_tensor = torch.tensor(X, dtype=torch.float32)
-        X_tensor = torch.tensor(df.to_numpy(), dtype=torch.float32)
-
-
-        return X_tensor, y
+    def preprocess(self, df):
+        df['sex'] = self.le.fit_transform(df['sex'])
+        x = self.process_features(df)
+        return x
     
     def predict_aki(self, measurement_vector):
-
-        """
-         Args:
-            mrn (_type_): _description_
-            measurements (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        Use a trained model to create label predictions on input data  
-
-        Arguments:
-            - filepath {string} -- csv training path
-            - model -- Trained model
-            - expected_columns {list} -- Expected columns based on the training data
-                
-
-        Returns:
-            - {numpy array}  -- predictions
-            - {list}  -- Expected labels (when we have a ground truth). 
-              
-            """
-        # Preprocess then run on trained model 
-        X_test, y = self.test_preprocessing(measurement_vector, self.expected_columns)
-        
-        
-        
-        self.model.eval
-        with torch.no_grad():  
-            predictions = self.model(X_test)
-
-        predictions = predictions.detach().numpy()
-        logging.info(f"prediction: {predictions}")
-        
-        # Convert to binary
-        return int((predictions.item()) > 0.3) # Lower than usual because we care more about F3 
+        x = self.preprocess(measurement_vector)
+        y = self.aki_model.predict(x)
+        return y
         
     def run(self):
         (mrn, test_time, patient_vector) = self.predict_queue.pop(0) # THIS IS SUPER INEFFICIENT LATER CHANGE USEAGE OF TYPE OF QUEUE FOR SPEED
@@ -173,6 +119,7 @@ if __name__=="__main__":
     df = pd.read_csv("test.csv")
     # Extract true AKI labels and convert 'y' to 1, 'n' to 0
     aki_data = np.where(df['aki'] == 'y', 1, 0).astype(int)
+    df = df.drop(columns="aki")
 
     # Save as CSV without column name
     #np.savetxt("aki_labels.csv", aki_data, fmt='%d', delimiter=',')
@@ -187,8 +134,10 @@ if __name__=="__main__":
 
     # Load saved labels without headers
     #data = np.loadtxt('aki_labels.csv', delimiter=',', dtype=int)
+    print(f"1s: {sum(predictions)}, length:{len(predictions)}")
 
     # Compute F3 Score
     f3_score_test = fbeta_score(aki_data, predictions, beta=3, zero_division=1)
 
     print('Final F3 score:', f3_score_test)
+
