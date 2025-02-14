4b695097ffff962ce972d1f6a265f41f05e249f1
Lefeuve Vincent <vincent.lefeuve@student-cs.fr>
2025-02-14 12:10:54 +0000

    Improved code quality, caught right exceptions

--- src/mysql_database.py
+++ src/mysql_database.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, Float, String, DateTime, ForeignKey, Enum
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker, declarative_base #Safer, automatic sanitizing
 from datetime import datetime
 from database import Database
@@ -20,9 +21,16 @@ class Measurement(Base):
 
 class MySQLDatabase(Database):
     def __init__(self, host, port, user, password, db):
-        database_uri = "mysql+pymysql://root:password@127.0.0.1:3306/hospital_db"
-        self.engine = create_engine(database_uri, echo=True)
-        self.Session = sessionmaker(bind=self.engine)
+        database_uri = f"mysql+pymysql://{user}:{password}@{host}:{port}/{db}"
+        
+        try:
+            self.engine = create_engine(database_uri, echo=True)
+            self.Session = sessionmaker(bind=self.engine)
+            print("MySQL database object created.")
+        except SQLAlchemyError as e:
+            print(f"Error initializing database connection: {e}")
+    
+        
 
     def connect(self):
         self.session = self.Session()
@@ -38,18 +46,18 @@ class MySQLDatabase(Database):
             self.session.add(patient)
             self.session.commit()
             print(f"Added patient with MRN {mrn}.")
-        except Exception as e:
+        except SQLAlchemyError as e:
             self.session.rollback()
             print(f"Error adding patient: {e}")
 
     def add_measurement(self, mrn: str, measurement_value: float, measurement_date=None) -> None:
         try:
-            measurement_date = measurement_date or datetime.utcnow()
+            measurement_date = measurement_date or datetime.now(datetime.zone.utc)  
             measurement_entry = Measurement(mrn=mrn, measurement_value=measurement_value, measurement_date=measurement_date)
             self.session.add(measurement_entry)
             self.session.commit()
             print(f"Added measurement for MRN {mrn}.")
-        except Exception as e:
+        except SQLAlchemyError as e:
             self.session.rollback()
             print(f"Error adding measurement: {e}")
 
@@ -57,12 +65,12 @@ class MySQLDatabase(Database):
         try:
             data = self.session.query(Measurement).filter_by(mrn=mrn).all()
             return [(entry.measurement_value, entry.measurement_date) for entry in data]
-        except Exception as e:
+        except SQLAlchemyError as e:
             print(f"Error retrieving data: {e}")
             return []
 
 if __name__ == "__main__":
-    db = MySQLDatabase("localhost","3306","root","password","hospital_db")
+    db = MySQLDatabase("localhost","3306","root","passwosrd","hospital_db")
     db.connect()
     print(db.get_data(100005546))
     #TODO: Unit tests
