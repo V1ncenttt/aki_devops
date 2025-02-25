bb31abe87e29fa10d6a09467576707afbe48c77b
Breznik, Zala <zala.breznik24@imperial.ac.uk>
2025-02-25 09:04:51 +0000

    Added a new metric to metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -7,6 +7,7 @@ BLOOD_TEST_RESULTS_RECEIVED = Counter('blood_test_results_received_total', 'Tota
 AKI_PAGES_SENT = Counter('aki_pages_sent_total', 'Total number of AKI event pages sent')
 AKI_PAGES_FAILED = Counter('aki_pages_failed_total', 'Total number of failed AKI event pages')
 PREDICTIONS_MADE = Counter('aki_predictions_total', 'Total number of AKI predictions made')
+PREDICTIONS_FAILED = Counter('aki_predictions_failed', 'Total number of AKI predictions that failed')
 POSITIVE_PREDICTIONS_MADE = Counter('aki_positive_predictions_total', 'Total number of positive AKI predictions made')
 MLLP_RECONNECTIONS = Counter('mllp_reconnections_total', 'Total number of mllp reconnection attempts to the socket')
 SYSTEM_UPTIME = Gauge('system_uptime_seconds', 'Time since the system started')
